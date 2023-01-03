1673baed13c9ebff4fb247eb5f849f9160eaffdc
ntekath <103177597+ntekath@users.noreply.github.com>
2023-01-03 16:03:41 +0100

    - message class

--- Turtel-App/ServerApp/Message/Domain/Message.cs
+++ Turtel-App/ServerApp/Message/Domain/Message.cs
@@ -1,6 +1,15 @@
-﻿namespace Turtel_App.ServerApp.Message.Domain
-{
-    public class Message
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Turtel_App.ServerApp.Message.Domain;
+
+public class Message
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public int SenderId { get; set; }
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
     }
-}
+    
--- Turtel-App/ServerApp/Message/Domain/MessageRepository.cs
+++ Turtel-App/ServerApp/Message/Domain/MessageRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace Turtel_App.ServerApp.Message.Domain
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory.Storage.Internal;
+using Turtel_App.ServerApp.Certification;
+
+namespace Turtel_App.ServerApp.Message.Domain
 {
-    public class MessageRepository
+    public class MessageRepository : InMemoryDbContext
     {
+        public MessageRepository() : base()
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public Message? FindByGuid(Guid guid) =>
+            (from Message Message in Messages
+                where Message.Id == guid
+                select Message).FirstOrDefault();
     }
 }
